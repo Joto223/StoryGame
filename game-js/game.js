8bb305b1efb951e558a117795a8497812b1cc251
Mac User <macuser@BlackBox.lan1>
2019-02-05 21:24:49 -0600

    fade in/out working, choice managment in progress

--- game-js/game.js
+++ game-js/game.js
@@ -1,9 +1,30 @@
 /*----- constants -----*/
+const NAME = document.getElementsByClassName('name');
+const start = document.querySelector('#start')
 /*----- app's state (variables) -----*/
+const gameTxt = document.getElementById('gameTxt');
+var $choice1 = $('#choice1').on('click', () => {})
+var $choice2 = $('#choice2').on('click', () => {})
+
+
 /*----- cached element references -----*/
-/*----- event listeners -----*/
+/*----- event listeners -----*/ start.addEventListener('click', game)
+
+
 /*----- functions -----*/
 function game(){
-alert('starting game...');
-document.getElementById('gameTxt').innerHTML = "The persistent patter of rain muffles the crunch of gravel as Vega traverses the outskirts of the city."
+  showChoice();
+$(".nameIn").fadeOut(3000, function(){
+  $('#gameTxt').fadeIn(5000).html('The persistent patter of rain muffles the crunch of gravel as Vega traverses the outskirts of the city.');
+  $('.nameIn').fadeIn(10000).html('Upon approaching his destination, Vega considers his objective');
+});
+// if ()
+
+}
+
+function showChoice() {
+  $('.choice').show();
+}
+function hideChoice() {
+  $('.choice').hide()
 }
